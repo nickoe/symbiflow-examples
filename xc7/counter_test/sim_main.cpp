94fa36c0d1c8734bdab8dc1f4009a27cea00e82a
Nick Østergaard <oe.nick@gmail.com>
2021-01-23 16:59:43 +0100

    More verilator mocking around
    verilator  --trace --cc counter.v BUFG.v --exe --build sim_main.cpp
    
    Build errors...

--- xc7/counter_test/sim_main.cpp
+++ xc7/counter_test/sim_main.cpp
@@ -1,9 +1,88 @@
+// DESCRIPTION: Verilator: Verilog example module
+//
+// This file ONLY is placed under the Creative Commons Public Domain, for
+// any use, without warranty, 2017 by Wilson Snyder.
+// SPDX-License-Identifier: CC0-1.0
+//======================================================================
+
+// For std::unique_ptr
+#include <memory>
+
+// Include common routines
+#include <verilated.h>
+
+// Include model header, generated from Verilating "counter.v"
 #include "Vcounter.h"
-#include "verilated.h"
+
+// Current simulation time (64-bit unsigned)
+vluint64_t main_time = 0;
+// Called by $time in Verilog
+double sc_time_stamp() {
+    return main_time;  // Note does conversion to real, to match SystemC
+}
+
 int main(int argc, char** argv, char** env) {
-Verilated::commandArgs(argc, argv);
-Vcounter* top = new Vcounter;
-while (!Verilated::gotFinish()) { top->eval(); }
-	delete top;
-	exit(0);
+    // This is a more complicated example, please also see the simpler examples/make_hello_c.
+
+    // Prevent unused variable warnings
+    if (false && argc && argv && env) {}
+
+    // Set debug level, 0 is off, 9 is highest presently used
+    // May be overridden by commandArgs
+    Verilated::debug(0);
+
+    // Randomization reset policy
+    // May be overridden by commandArgs
+    Verilated::randReset(2);
+
+    // Verilator must compute traced signals
+    Verilated::traceEverOn(true);
+
+    // Pass arguments so Verilated code can see them, e.g. $value$plusargs
+    // This needs to be called before you create any model
+    Verilated::commandArgs(argc, argv);
+
+    // Create logs/ directory in case we have traces to put under it
+    Verilated::mkdir("logs");
+
+    // Construct the Verilated model, from Vtop.h generated from Verilating "top.v"
+    // Using unique_ptr is similar to "Vtop* top = new Vtop" then deleting at end
+    const std::unique_ptr<Vcounter> top{new Vcounter};
+
+    VerilatedVcdC* tfp = new VerilatedVcdC;
+    top->trace(tfp, 99);  // Trace 99 levels of hierarchy
+    tfp->open("trace.vcd");
+
+    // Set some inputs
+    //top->counter = 0;
+    top->clk = 0;
+
+    // Simulate until $finish
+    while (!Verilated::gotFinish()) {
+        main_time++;  // Time passes...
+
+        // Toggle a fast (time/2 period) clock
+        top->clk = !top->clk;
+        tfp->dump(main_time);
+        // Evaluate model
+        // (If you have multiple models being simulated in the same
+        // timestep then instead of eval(), call eval_step() on each, then
+        // eval_end_step() on each.)
+        top->eval();
+
+        // Read outputs
+        VL_PRINTF("[%" VL_PRI64 "d] clk=%x led=%x\n", main_time, top->clk, top->led);
+    }
+
+    // Final model cleanup
+    top->final();
+
+    //  Coverage analysis (since test passed)
+#if VM_COVERAGE
+    Verilated::mkdir("logs");
+    VerilatedCov::write("logs/coverage.dat");
+#endif
+
+    // Return good completion status
+    exit(0);
 }
